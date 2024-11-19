892da60920b5d3475577dca80017020d6971b824
TadaEXE <nevioxbl@gmail.com>
2024-11-19 18:54:48 +0100

    Setup LLVM

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 #include <algorithm>
+#include <memory>
+
+#include <llvm/ExecutionEngine/ExecutionEngine.h>
+#include <llvm/ExecutionEngine/GenericValue.h>
+#include <llvm/IR/IRBuilder.h>
+
+#include <fstream>
 
 char *getCmdOption(char **begin, char **end, const std::string &option)
 {
@@ -16,6 +23,41 @@ bool cmdOptionExists(char **begin, char **end, const std::string &option)
     return std::find(begin, end, option) != end;
 }
 
+int test_llvm_usage()
+{
+    auto context = std::make_unique<llvm::LLVMContext>();
+    llvm::IRBuilder<> builder(*context);
+
+    auto module = std::make_unique<llvm::Module>("hello", *context);
+
+    // build a 'main' function
+    auto i32 = builder.getInt32Ty();
+    auto prototype = llvm::FunctionType::get(i32, false);
+    llvm::Function *main_fn = llvm::Function::Create(prototype, llvm::Function::ExternalLinkage, "main", module.get());
+    llvm::BasicBlock *body = llvm::BasicBlock::Create(*context, "body", main_fn);
+    builder.SetInsertPoint(body);
+
+    // use libc's printf function
+    auto i8p = builder.getPtrTy();
+    auto printf_prototype = llvm::FunctionType::get(i8p, true);
+    auto printf_fn = llvm::Function::Create(printf_prototype, llvm::Function::ExternalLinkage, "printf", module.get());
+
+    // call printf with our string
+    auto format_str = builder.CreateGlobalString("hello world\n");
+    builder.CreateCall(printf_fn, {format_str});
+
+    // return 0 from main
+    auto ret = llvm::ConstantInt::get(i32, 0);
+    builder.CreateRet(ret);
+
+    // write to output file
+    std::error_code EC;
+    llvm::raw_fd_ostream llvmOut("output.ll", EC);
+    module->print(llvmOut, nullptr);
+
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     if (!cmdOptionExists(argv, argv + argc, "-f"))
@@ -30,4 +72,6 @@ int main(int argc, char *argv[])
     {
         std::cout << "File: " << filename << std::endl;
     }
+
+    return test_llvm_usage();
 }
