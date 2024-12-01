14185d5affa85c86de485187010bc2233ec70300
TadaEXE <nevioxbl@gmail.com>
2024-12-02 00:28:16 +0100

    Formatted strings for output manager

--- src/util/output-manager/src/output-manager/ioutput-manager.h
+++ src/util/output-manager/src/output-manager/ioutput-manager.h
@@ -2,6 +2,7 @@
 
 #include <functional>
 #include <string>
+#include <vector>
 
 namespace util::output
 {
@@ -11,19 +12,43 @@ namespace util::output
         High,
         Mid,
         Low,
+        UltraLow
     };
 
     struct OutputMessage
     {
     public:
-        const std::string& msg;
+        const std::string msg;
         Verbosity_Level verbosity;
+
+        OutputMessage(Verbosity_Level level, const std::string &message)
+            : msg(message), verbosity(level) {}
+
+        template <typename... Args>
+        OutputMessage(Verbosity_Level level, const std::string &format, Args... args)
+            : verbosity(level)
+        {
+            size_t size = snprintf(nullptr, 0, format.c_str(), args...) + 1; // Extra space for '\0'
+            std::vector<char> buf(size);
+            snprintf(buf.data(), size, format.c_str(), args...);
+            msg = std::string(buf.data(), buf.size() - 1);
+        }
+
+        template <typename... Args>
+        static OutputMessage create(Verbosity_Level level, const std::string &format, Args... args)
+        {
+            size_t size = snprintf(nullptr, 0, format.c_str(), args...) + 1; // Extra space for '\0'
+            std::vector<char> buf(size);
+            snprintf(buf.data(), size, format.c_str(), args...);
+            return OutputMessage(level, std::string(buf.data(), buf.size() - 1));
+        }
     };
+
 
     class IOutputManager
     {
     public:
-        virtual void append_msg(const OutputMessage& msg) = 0;
+        virtual void append_msg(const OutputMessage &msg) = 0;
 
         virtual ~IOutputManager() = default;
     };
